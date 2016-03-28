9eeaf4db09fc629787e31a10d19db0eab9eb7fae
Nick <npessinis@team3467.org>
2016-03-28 15:58:55 -0400

    idfk

--- src/org/usfirst/frc/team3467/robot/subsytems/HealthAnalysis/HealthAnalysis.java
+++ src/org/usfirst/frc/team3467/robot/subsytems/HealthAnalysis/HealthAnalysis.java
@@ -5,5 +5,21 @@ import org.usfirst.frc.team3467.robot.commands.CommandBase;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 public class HealthAnalysis {
-private PowerDistributionPanel health = CommandBase.brownout.getpdp();
+private PowerDistributionPanel health = CommandBase.brownout.getPDP();
+
+	double shooterCurrent;
+	double driveCurrent;
+	double intakerollerCurrent;
+	double flashlightCurrent;
+	double totalVoltage;
+	double totalCurrent;
+	double shooterTemperature;
+	
+	public void update(){
+		shooterCurrent = health.getCurrent(0);
+		driveCurrent = health.getCurrent(0);
+		intakerollerCurrent = health.getCurrent(0);
+		flashlightCurrent = health.getCurrent(0);
+		
+	}
 }
